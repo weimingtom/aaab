75895ed919d77f1285228caf24e850da934ae70b
huyao919@gmail.com <huyao919@gmail.com@8dd2a1b1-ac81-b2e0-4851-c90cabaaaa8f>
2012-03-19 14:53:59 +0000

    修改格式

--- trunk/dongman/Lib/Action/Admin/IndexAction.class.php
+++ trunk/dongman/Lib/Action/Admin/IndexAction.class.php
@@ -1,39 +1,39 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends BaseAction{
-    public function index(){
-        $this->display('./Public/admin/index.html');
-    }
-    public function top(){
-        $this->display('./Public/admin/top.html');
-    }
-    public function left(){
-        $this->display('./Public/admin/left.html');
-    }
-    public function main(){
+	public function index(){
+		$this->display('./Public/admin/index.html');
+	}
+	public function top(){
+		$this->display('./Public/admin/top.html');
+	}
+	public function left(){
+		$this->display('./Public/admin/left.html');
+	}
+	public function main(){
 		$rs = D("Vod");
 		$img = array();
 		$imghttp=$rs->where('Left(vod_pic,7)="http://" and vod_del=0')->count();
 		$imgfail=$rs->where('Left(vod_pic,8)="httpf://" and vod_del=0')->count();
 		$this->assign('imghttp',$imghttp);$this->assign('imgfail',$imgfail);
-        $this->display('./Public/admin/main.html');
-    }
-	
+		$this->display('./Public/admin/main.html');
+	}
+
 	// 配置信息
-    public function config(){
+	public function config(){
 		$tpl=TMPL_PATH.'*';$list=glob($tpl);
 		foreach ($list as $i=>$file){
 			$dir[$i]['filename']=basename($file);
 		}
 		$this->assign('dir',$dir);
-	    $this->ppvod_list();//更新导航菜单
+		$this->ppvod_list();//更新导航菜单
 		$config=require './Conf/setting.php';
 		$this->assign($config);
-        $this->display('./Public/admin/config.html');
-    }
-	
+		$this->display('./Public/admin/config.html');
+	}
+
 	// 配置信息保存
-    public function configsave(){
+	public function configsave(){
 		$config=$_POST["config"];
 		$config['site_tongji']=stripslashes($config['site_tongji']);
 		$config['play_collect_content']=stripslashes($config['play_collect_content']);
@@ -45,7 +45,7 @@ class IndexAction extends BaseAction{
 		$config['play_collect']=(bool) $config['play_collect'];
 		$config['tmpl_cache_on']=(bool) $config['tmpl_cache_on'];
 		$config['html_cache_on']=(bool) $config['html_cache_on'];
-		$config['user_gbcm']=(bool) $config['user_gbcm'];	
+		$config['user_gbcm']=(bool) $config['user_gbcm'];
 		$config['play_second']=intval($config['play_second']);
 		//$arr1 = array('\"',"\'");
 		//$arr2 = array('','');
@@ -61,7 +61,7 @@ class IndexAction extends BaseAction{
 			$arrlist[trim($key)]=trim($val);
 		}
 		$config["play_list"]=$arrlist;
-		//	
+		//
 		foreach(explode(chr(13),trim($config["play_collect_content"])) as $v){
 			$arrcollect[]=trim($v);
 		}
@@ -70,28 +70,28 @@ class IndexAction extends BaseAction{
 		$config['html_cache_time']=$config['html_cache_time']*3600;//其它页缓存
 		if($config['html_cache_index']>0){$config['_htmls_']['home:index:index']=array('{:action}',$config['html_cache_index']*3600);}else{$config['_htmls_']['home:index:index']=NULL;}
 		if($config['html_cache_list']>0){
-		    $config['_htmls_']['home:vod:show']=array('{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',$config['html_cache_list']*3600);
+			$config['_htmls_']['home:vod:show']=array('{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',$config['html_cache_list']*3600);
 			$config['_htmls_']['home:news:show']=array('{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',$config['html_cache_list']*3600);
 		}else{
-		    $config['_htmls_']['home:vod:show']=NULL;
+			$config['_htmls_']['home:vod:show']=NULL;
 			$config['_htmls_']['home:news:show']=NULL;
 		}
 		if($config['html_cache_content']>0){
-		    $config['_htmls_']['home:vod:read']=array('{:module}_{:action}/{id|md5}',$config['html_cache_content']*3600);
+			$config['_htmls_']['home:vod:read']=array('{:module}_{:action}/{id|md5}',$config['html_cache_content']*3600);
 			$config['_htmls_']['home:news:read']=array('{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',$config['html_cache_content']*3600);
 		}else{
-		    $config['_htmls_']['home:vod:read']=NULL;
+			$config['_htmls_']['home:vod:read']=NULL;
 			$config['_htmls_']['home:news:read']=NULL;
 		}
 		if($config['html_cache_play']>0){
-		    $config['_htmls_']['home:vod:play']=array('{:module}_{:action}/{id|md5}',$config['html_cache_play']*3600);
+			$config['_htmls_']['home:vod:play']=array('{:module}_{:action}/{id|md5}',$config['html_cache_play']*3600);
 		}else{
-		    $config['_htmls_']['home:vod:play']=NULL;
-		}						
+			$config['_htmls_']['home:vod:play']=NULL;
+		}
 		if($config['html_cache_ajax']>0){
-		    $config['_htmls_']['home:ajax:show']=array('{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',$config['html_cache_ajax']*3600);
+			$config['_htmls_']['home:ajax:show']=array('{:module}_{:action}/{$_SERVER.REQUEST_URI|md5}',$config['html_cache_ajax']*3600);
 		}else{
-		    $config['_htmls_']['home:ajax:show']=NULL;
+			$config['_htmls_']['home:ajax:show']=NULL;
 		}
 		if(0==$config['url_html']){
 			@unlink(APP_PATH.c('site_path').'index'.c('html_file_suffix'));//动态模式则删除首页静态文件
@@ -119,33 +119,33 @@ class IndexAction extends BaseAction{
 		}
 		foreach(C('play_player') as $key=>$value){
 			$pp_play.= 'var play_'.$key.'="'.$value.'";';
-		}			
+		}
 		write_file('./Runtime/Player/play.js',$pp_play);
 		$this->success('恭喜您，配置信息更新成功！');
-    }	
-	
+	}
+
 	// 用户管理
-    public function show(){
-	    $rs=D("Admin.Admin");
+	public function show(){
+		$rs=D("Admin.Admin");
 		$list=$rs->order('admin_id desc')->select();
 		$this->assign('list',$list);
-        $this->display('./Public/admin/admin.html');
-    }
-	
+		$this->display('./Public/admin/admin.html');
+	}
+
 	// 用户添加
-    public function add(){
+	public function add(){
 		$array=array();
 		$id=$_GET['admin_id'];
 		if($id){
-		    $rs=D("Admin.Admin");
+			$rs=D("Admin.Admin");
 			$array=$rs->find($id);
 			$abc=explode(',',$array['admin_ok']);
 		}
 		$this->assign('admin',$abc);
 		$this->assign($array);
-        $this->display('./Public/admin/admin.html');
-    }
-	
+		$this->display('./Public/admin/admin.html');
+	}
+
 	//处理权限入库
 	public function _before_insert(){
 		$ok=$_POST['admin_ok'];
@@ -157,32 +157,32 @@ class IndexAction extends BaseAction{
 			}
 		}
 		$_POST['admin_ok']=implode(',',$rs);
-	} 
-	   	
+	}
+
 	// 写入数据
 	public function insert(){
 		$Form=D("Admin.Admin");
 		if($Form->where('admin_name="'.trim($_POST['admin_name']).'"')->find()){$this->error('该用户名已经存,在请重新填写！');};
 		if($Form->create()){
 			if(false!==$Form->add()){
-			    $this->assign("jumpUrl",'index.php?s=Admin-Index-Show');
+				$this->assign("jumpUrl",'index.php?s=Admin-Index-Show');
 				$this->success('数据添加成功！');
 			}else{
 				$this->error('数据写入错误');
 			}
 		}else{
-		    $this->error($Form->getError());
-		}		
+			$this->error($Form->getError());
+		}
 	}
-	
+
 	// 更新数据
 	public function update(){
-	    $this->_before_insert();
+		$this->_before_insert();
 		$Form=D("Admin.Admin");
 		if($Form->create()){
 			$list=$Form->save();
 			if($list!==false){
-			    $this->assign("jumpUrl",'index.php?s=Admin-Index-Show');
+				$this->assign("jumpUrl",'index.php?s=Admin-Index-Show');
 				$this->success('数据更新成功！');
 			}else{
 				$this->error("没有更新任何数据!");
@@ -191,20 +191,20 @@ class IndexAction extends BaseAction{
 			$this->error($Form->getError());
 		}
 	}
-	
+
 	// 删除数据
-    public function del(){
+	public function del(){
 		$rs=D("Admin.Admin");
 		$admin_id=$_GET['admin_id'];
 		$rs->where('admin_id='.$admin_id)->delete();
 		$this->success('成功删除一条ID为'.$admin_id.'的数据！');
-    }
-	
+	}
+
 	// 批量删除
-    public function delall(){
+	public function delall(){
 		$rs=D("Admin.Admin");
 		foreach($_POST['admin_id'] as $value){$rs->where('admin_id='.$value)->delete();}
 		$this->success('批量删除数据成功！');
-    }				
+	}
 }
 ?>
--- trunk/dongman/Lib/Action/HomeAction.class.php
+++ trunk/dongman/Lib/Action/HomeAction.class.php
@@ -1,9 +1,9 @@
 <?php
 class HomeAction extends AllAction{
-    //前台公用文件
-    public function _initialize(){
-	    $style=array();
-        header("Content-Type:text/html; charset=".C(DEFAULT_CHARSET));	
+	//前台公用文件
+	public function _initialize(){
+		$style=array();
+		header("Content-Type:text/html; charset=".C(DEFAULT_CHARSET));
 		$style['tpl']=C('site_path').TEMPLATE_PATH.'/';
 		$style['css']='<link rel="stylesheet" type="text/css" href="'.C('site_path').TEMPLATE_PATH.'/style.css">'."\n";
 		$style['root']=C('site_path');//APP_PATH.'/'
@@ -17,13 +17,13 @@ class HomeAction extends AllAction{
 		$style['icp']=C('site_icp');
 		$style['model']=strtolower(MODULE_NAME);
 		$style['action']=strtolower(ACTION_NAME);
-		C('TOKEN_NAME','__ppvod__');	
-        $this->assign($style);
+		C('TOKEN_NAME','__ppvod__');
+		$this->assign($style);
 		$this->assign('pplink',F('_ppvod/link'));
 		$this->assign('ppmenu',F('_ppvod/listtree'));
-    }	
+	}
 	public function showmenu(){
-	    return F('_ppvod/listtree');
+		return F('_ppvod/listtree');
 	}
 }
 ?>
