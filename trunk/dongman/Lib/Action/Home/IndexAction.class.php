4f2f6c39595b1ef760dbcf9e0024571ddd0d3252
huyao919 <huyao919@8dd2a1b1-ac81-b2e0-4851-c90cabaaaa8f>
2012-04-06 06:58:45 +0000

    修改细节

--- trunk/dongman/Lib/Action/Home/IndexAction.class.php
+++ trunk/dongman/Lib/Action/Home/IndexAction.class.php
@@ -12,10 +12,10 @@ class IndexAction extends HomeAction{
 		// 昨天
 		$yesterdaystart = strtotime(date('Y-m-d', strtotime('-1 day')));
 		$yesterdayend = $yesterdaystart + 86400;
-		$vodlist['yesterday'] = $vod->field("vod_name")->where("vod_addtime>='$yesterdaystart' AND vod_addtime<='$yesterdayend'")->limit('22')->select();
+		$vodlist['yesterday'] = $vod->field("vod_name")->where("vod_updatetime>='$yesterdaystart' AND vod_updatetime<='$yesterdayend'")->limit('22')->select();
 		$yesterdaystart2 = $yesterdaystart - 86400;
 		$yesterdayend2 = $yesterdaystart;
-		$vodlist['yesterday2'] = $vod->field("vod_name,vod_url,vod_color")->where("vod_addtime>='$yesterdaystart2' AND vod_addtime<='$yesterdayend2'")->limit('22')->select();
+		$vodlist['yesterday2'] = $vod->field("vod_name,vod_url,vod_color")->where("vod_updatetime>='$yesterdaystart2' AND vod_updatetime<='$yesterdayend2'")->limit('22')->select();
 		
 		$this->assign('title',C('site_name').'-首页'.C('site_by'));
 		$this->assign('pplist', A("Home"));
