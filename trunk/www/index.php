61a81dc9f88e23cbf2a67c70590f0b5f6350a4b5
huyao919@gmail.com <huyao919@gmail.com@8dd2a1b1-ac81-b2e0-4851-c90cabaaaa8f>
2012-04-11 13:27:23 +0000

    修改9大陆首页

--- trunk/www/index.php
+++ trunk/www/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 define('WWW.MYDECMS.COM',true);
 include 'webadmin/include/config.inc.php';
 ?>
@@ -240,6 +240,17 @@ include 'webadmin/include/config.inc.php';
         <!--右侧内容结束-->
         
     
+	</div>
+	<div id="friends">
+		<div class="friend_title"><h1>　友情链接</h1></div>
+		<div class="friend_text">
+			<?php
+			$links = $mysql->fetch_all("SELECT * FROM -table-link ORDER BY id ASC");
+			foreach ($links as $v) {
+			?>
+			<li><a href="<?php echo $v['url']?>" target="_blank"><?php echo $v['name']?></a></li>
+			<?php }?>
+		</div>
 	</div>
 	<!--内容结束-->
 	<?php include("footer.php");?>
