a569c568c1466b573483cb18aa86f5609f447755
senniao@126.com <senniao@126.com@8dd2a1b1-ac81-b2e0-4851-c90cabaaaa8f>
2012-08-05 15:51:19 +0000

    修改SQL

--- trunk/btbbt_xiuno/sql/alter_huyao.sql
+++ trunk/btbbt_xiuno/sql/alter_huyao.sql
@@ -1,27 +1,27 @@
 #2012.7.28
 drop table if exists `bbs_medal`;
 CREATE TABLE `bbs_medal` (                                                   
-`medalId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '勋章ID',       
-`medalName` varchar(30) NOT NULL COMMENT '勋章名称',                       
+`medalid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '勋章ID',       
+`medalname` varchar(30) NOT NULL COMMENT '勋章名称',                       
 `description` varchar(255) NOT NULL COMMENT '勋章描述',                    
 `picture` varchar(255) NOT NULL COMMENT '图片地址',                        
-`receiveType` tinyint(1) NOT NULL DEFAULT '0' COMMENT '领取方式',          
+`receivetype` tinyint(1) NOT NULL DEFAULT '0' COMMENT '领取方式',          
 `seq` tinyint(1) NOT NULL DEFAULT '0' COMMENT '排序',                      
-`userId` int(10) NOT NULL DEFAULT '0' COMMENT '用户ID',                    
-`createdTime` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',             
-PRIMARY KEY (`medalId`)                                                    
+`uid` int(10) NOT NULL DEFAULT '0' COMMENT '用户ID',                    
+`createdtime` int(11) NOT NULL DEFAULT '0' COMMENT '创建时间',             
+PRIMARY KEY (`medalid`)                                                    
 ) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='勋章信息表';
 
 drop table if exists `bbs_medal_user`;
 CREATE TABLE `bbs_medal_user` (                                                 
-	`medalUserId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '发放ID',      
-	`medalId` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '勋章ID',             
-	`userName` char(30) NOT NULL COMMENT '用户名',                                
-	`userId` int(10) NOT NULL DEFAULT '0' COMMENT '获取勋章用户ID',               
-	`receiveType` tinyint(1) NOT NULL DEFAULT '0' COMMENT '领取方式',             
-	`expiredTime` int(11) NOT NULL DEFAULT '0' COMMENT '过期时间',                
-	`isApply` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否用户申请',             
-	`fuserId` int(10) NOT NULL DEFAULT '0' COMMENT '发出者用户ID',                
-	`createdTime` int(10) NOT NULL DEFAULT '0' COMMENT '创建时间',                
-	PRIMARY KEY (`medalUserId`)                                                   
+	`muid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '发放ID',      
+	`medalid` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '勋章ID',
+	`uid` int(10) NOT NULL DEFAULT '0' COMMENT '获取勋章用户ID',            
+	`username` char(30) NOT NULL COMMENT '用户名',                                
+	`receivetype` tinyint(1) NOT NULL DEFAULT '0' COMMENT '领取方式',             
+	`expiredtime` int(11) NOT NULL DEFAULT '0' COMMENT '过期时间',                
+	`isapply` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否用户申请',             
+	`fuid` int(10) NOT NULL DEFAULT '0' COMMENT '发出者用户ID',                
+	`createdtime` int(10) NOT NULL DEFAULT '0' COMMENT '创建时间',                
+	PRIMARY KEY (`muid`)                                                   
 ) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='用户所得勋章表';
