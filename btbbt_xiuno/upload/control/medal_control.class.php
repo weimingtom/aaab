f7b508e2d246b6cd67eb18988ebaebf6074adc17
huyao919 <huyao919@8dd2a1b1-ac81-b2e0-4851-c90cabaaaa8f>
2012-08-15 04:53:14 +0000

    开发勋章

--- btbbt_xiuno/upload/control/medal_control.class.php
+++ btbbt_xiuno/upload/control/medal_control.class.php
@@ -14,8 +14,22 @@ class medal_control extends common_control {
 		parent::__construct();
 	}
 	
-	public function on_list() {
-		$medallist = $this->medal->get_medallist();
+	// 勋章中心
+	public function on_list() 
+	{
+		$user = $this->user->get($this->_user['uid']);
+		$medallist = $medallist = $this->medal->index_fetch(array(), array('seq'=>1));
+		foreach ($medallist as &$medal) {
+			$medal['receive'] = '';
+			if ($medal['receivetype'] == 1 && $user) {
+				$isreceive = $this->medal->is_receive($user, $medal);
+				if ($isreceive) {
+					$medal['receive'] = '<a href="?medal-receive.htm?uid='.$user['uid'].'&medalid='.$medal['medalid'].'">领取</a>';
+				}
+			}
+		}
+		$this->medal->medallist_form($medallist);
+		
 		$this->view->assign('medallist', $medallist);
 		$this->view->display('medal_list.htm');
 	}
@@ -27,6 +41,33 @@ class medal_control extends common_control {
 		
 		$this->view->assign('medaluserlist', $medaluserlist);
 		$this->view->display('medal_my.htm');
+	}
+	
+	// 领取
+	public function on_receive() {
+		$uid = core::gpc('uid');
+		$medalid = core::gpc('medalid');
+		if ($this->_user['uid'] == $uid && $medalid) {
+			$user = $this->user->get($uid);
+			$medal = $this->medal->get($medalid);
+			$medaluser = $this->medal_user->get_medaluser_by_uid_medalid($uid, $medalid);
+			if (!$medaluser && $medal && $user) {
+				$this->medal_user->create(array(
+					'medalid'=>$medalid,
+					'uid'=>$uid,
+					'username'=>$user['username'],
+					'receivetype'=>$medal['receivetype'],
+					'autogrant'=>$medal['autogrant'],
+					'expiredtime'=>0,
+					'isapply'=>0,
+					'fuid'=>$uid,
+					'createdtime'=>time(),
+				));
+			}
+			$this->message('领取成功，请等待管理员审核。', true, "?medal-list.htm");
+		} else {
+			$this->message('领取失败，请联系管理员。', true, "?medal-list.htm");
+		}
 	}
 }
 
