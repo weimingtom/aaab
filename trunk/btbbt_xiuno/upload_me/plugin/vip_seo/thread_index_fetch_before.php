915d6994432cf1ba9008fb228a566331c175c4df
huyao919 <huyao919@8dd2a1b1-ac81-b2e0-4851-c90cabaaaa8f>
2012-08-16 10:18:13 +0000

    开发勋章插件

---
+++ trunk/btbbt_xiuno/upload_me/plugin/vip_seo/thread_index_fetch_before.php
@@ -0,0 +1,9 @@
+		// SEO 优化，商业版本使用
+		$this->_title = array();
+		if($thread['seo_keywords']) {
+			$this->_title[] = $thread['seo_keywords'];
+			$this->_seo_keywords = $thread['subject'];
+		} else {
+			$this->_title[] = $thread['subject'];
+			$this->_seo_keywords = $thread['subject'];
+		}
