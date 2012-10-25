ee434bcfb698b33c71e6e2555b8efa0024674f9f
huyao919 <huyao919@8dd2a1b1-ac81-b2e0-4851-c90cabaaaa8f>
2012-10-25 09:30:04 +0000

    增加广告系统

---
+++ btbbt_xiuno/upload_me/plugin/btbbt_ad/conf.php
@@ -0,0 +1,22 @@
+<?php
+
+return 	array (
+	'enable' => 1,			// 是否启用
+	'installed' => 1,		// 已经安装
+	'name'=>'BT之家广告插件',		// 插件名
+	'brief'=>'xiuno一起努力',
+	'version'=>'1.0',		// 插件版本
+	'bbs_version'=>'2.*',		// 插件支持的 Xiuno BBS 版本
+	'icon'=>'',			// 插件的LOGO
+	'author'=>'huyao',		// 插件的作者
+	'author_homepage'=>'http://www.xiuno.com/',	// 插件作者的主页
+	'email'=>'252288762@qq.com',	// 插件的联系EMAIL
+	'verify_code' => '',		// 经过官方安全认证的标志。
+	'stars' => 3,			// 官方对插件的评级，1-5星级。
+	'setting_url' => '',		// 配置的URL。
+
+        // 接口 相关 --------->
+        'api_key' => 'ETBS56@134@!',
+        'api_url' => 'http://www.9dalu.com',
+);
+?>
